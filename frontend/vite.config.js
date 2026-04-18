8b9c641b20fdffcd31aad8218e4236ec2480ddfd
niux3 <renaudbourdeau@gmail.com>
2026-04-18 19:18:20 +0200

    feat: vite build in static folder

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,31 @@
+// frontend/vite.config.js
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  base: '',
+    base: '',
 
-  build: {
-    emptyOutDir: true,
-    assetsDir: 'assets',
-  }
+    build: {
+        emptyOutDir: true,
+
+        rollupOptions: {
+            input: resolve(__dirname, 'src/main.js'),
+
+            output: {
+                // Désactive la génération de HTML
+                entryFileNames: 'js/[name].js',
+                chunkFileNames: 'js/[name].js',
+                assetFileNames: (assetInfo) => {
+                    // CSS dans css/, autres assets dans assets/
+                    if (assetInfo.name?.endsWith('.css')) {
+                        return 'css/[name].[ext]'
+                    }
+                    return 'assets/[name].[ext]'
+                }
+            }
+        },
+
+        // Dossier de sortie vers ton backend Django
+        outDir: '../backend/apps/core/static'
+    }
 })
