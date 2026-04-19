c5dfab38708b7fcb557e48023ce854dcaa1310ef
niux3 <renaudbourdeau@gmail.com>
2026-04-19 13:55:03 +0200

    feat: default values in form

--- backend/apps/candidacy/forms.py
+++ backend/apps/candidacy/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from candidacy.models import Candidacy
+from candidacy.models import Candidacy, Civility, Status, Position
 
 
 class CandidacyForm(forms.ModelForm):
@@ -10,3 +10,29 @@ class CandidacyForm(forms.ModelForm):
             'civilities': forms.CheckboxSelectMultiple(),
             'technologies': forms.CheckboxSelectMultiple(),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if not self.initial.get('status'):
+            try:
+                self.initial['status'] = Status.objects.get(
+                    name='lead développeur').pk
+            except Status.DoesNotExist:
+                ...
+
+        if not self.initial.get('position'):
+            try:
+                self.initial['position'] = Position.objects.get(
+                    name='fullstack').pk
+            except Position.DoesNotExist:
+                ...
+
+        if not self.initial.get('civilities'):
+            try:
+                self.initial['civilities'] = list(
+                    Civility.objects.filter(
+                        name__in=['Madame', 'Monsieur']).values_list('pk', flat=True)
+                )
+            except Civility.DoesNotExist:
+                ...
