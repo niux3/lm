8761b10251924719e101e41f7398a1a22bd13401
niux3 <renaudbourdeau@gmail.com>
2026-04-20 14:25:05 +0200

    feat: upgrade default values

--- backend/apps/candidacy/forms.py
+++ backend/apps/candidacy/forms.py
@@ -1,38 +1,53 @@
 from django import forms
-from candidacy.models import Candidacy, Civility, Status, Position
+from candidacy.models import (
+    Candidacy,
+    Civility,
+    Status,
+    Position,
+    Contract,
+    Technology
+)
 
 
 class CandidacyForm(forms.ModelForm):
     class Meta:
         model = Candidacy
         fields = '__all__'
-        widgets = {
-            'civilities': forms.CheckboxSelectMultiple(),
-            'technologies': forms.CheckboxSelectMultiple(),
-        }
+
+    civilities = forms.ModelMultipleChoiceField(
+        queryset=Civility.objects.all(),
+        widget=forms.CheckboxSelectMultiple(),
+        required=True,
+        initial=lambda: Civility.objects.filter(
+            name__in=['Madame', 'Monsieur']
+        ).values_list('pk', flat=True)
+    )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        if not self.initial.get('status'):
-            try:
-                self.initial['status'] = Status.objects.get(
-                    name='lead développeur').pk
-            except Status.DoesNotExist:
-                ...
+        self._get_default_value('status', 'lead développeur', Status)
+        self._get_default_value('contract', 'CDI', Contract)
+        self._get_default_value('position', 'fullstack', Position)
 
-        if not self.initial.get('position'):
-            try:
-                self.initial['position'] = Position.objects.get(
-                    name='fullstack').pk
-            except Position.DoesNotExist:
-                ...
+        if not self.initial.get('expertise'):
+            self.initial['expertise'] = "les applications web et embarquées"
 
-        if not self.initial.get('civilities'):
+        # valeur défaut manuel. Dans le template, les champs sont écrits manuellement
+        if not self.initial.get('technologies'):
             try:
-                self.initial['civilities'] = list(
-                    Civility.objects.filter(
-                        name__in=['Madame', 'Monsieur']).values_list('pk', flat=True)
+                self.initial['technologies'] = list(
+                    Technology.objects.filter(
+                        value__in=['Accessibilité', 'SEO', 'GNU/Linux']).values_list('pk', flat=True)
                 )
-            except Civility.DoesNotExist:
+                print(self.initial.get('technologies'))
+            except Technology.DoesNotExist:
+                ...
+
+    def _get_default_value(self, name, condition, model):
+        if not self.initial.get(name):
+            try:
+                self.initial[name] = model.objects.get(
+                    name=condition).pk
+            except model.DoesNotExist:
                 ...
